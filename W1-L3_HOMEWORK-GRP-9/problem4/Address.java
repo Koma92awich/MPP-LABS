fdd3231c40e7fb0a237ae199a1b85b41a6de8bed
thierry.manzi <thierry.manzi001@gmail.com>
2019-10-03 07:13:44 -0500

    Assignment 3

--- W1-L3_HOMEWORK-GRP-9/problem4/Address.java
+++ W1-L3_HOMEWORK-GRP-9/problem4/Address.java
@@ -6,10 +6,13 @@ public class Address {
 	private String state;
 	private String zip;
 	public Address(String street, String city, String state, String zip) {
-		super();
 		this.street = street;
 		this.city = city;
 		this.state = state;
 		this.zip = zip;
+	}
+	@Override
+	public String toString() {
+		return "Address [street=" + street + ", city=" + city + ", state=" + state + ", zip=" + zip + "]";
 	}
 }
--- W1-L3_HOMEWORK-GRP-9/problem4/Property.java
+++ W1-L3_HOMEWORK-GRP-9/problem4/Property.java
@@ -2,5 +2,11 @@ package lab3.problem4;
 
 public abstract class Property {
 	private Address address;
-	public abstract double computeRent();
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public abstract double computeRent();	
 }
