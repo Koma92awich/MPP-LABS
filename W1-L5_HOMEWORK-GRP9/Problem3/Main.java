65412a37ad19f5c2e2220c6951207f67c366326b
Koma92awich <koma92awich@gmail.com>
2019-10-11 10:40:52 -0500

    Update Main.java

--- W1-L5_HOMEWORK-GRP9/Problem3/Main.java
+++ W1-L5_HOMEWORK-GRP9/Problem3/Main.java
@@ -4,9 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		VehicleFactory vf = new VehicleFactory();
-		
-		Vehicle vec  = vf.startEngine("ElectricCar");
+		Vehicle vec  = VehicleFactory.startEngine("ElectricCar");
 		vec.startEngine();
 
 	}
