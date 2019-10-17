46c3368b2914cb21e3be4d0fdb9e8f47e6ad78ff
Koma92awich <koma92awich@gmail.com>
2019-10-16 22:23:11 -0500

    Add files via upload

---
+++ W3-L8-HOMEWORK/Problem3/Problem3A.java
@@ -0,0 +1,20 @@
+package lesson8Labs;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Problem3A {
+
+	public static void main(String[] args) {
+		
+		List<String> fruits = Arrays.asList("Apple", "Banana","Orange","Cherries","blums");
+		
+	fruits.forEach(x ->{
+		System.out.println(x);
+		});
+
+
+	
+	}
+
+}
---
+++ W3-L8-HOMEWORK/Problem3/Problem3B.java
@@ -0,0 +1,16 @@
+package lesson8Labs;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Problem3B {
+
+	public static void main(String[] args) {
+
+		List<String> fruits = Arrays.asList("Apple", "Banana", "Orange", "Cherries", "blums");
+
+		fruits.forEach(System.out::println);
+
+	}
+
+}
