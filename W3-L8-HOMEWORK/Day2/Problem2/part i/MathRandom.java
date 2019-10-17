6a5f2b8efabb57144f4cb0a007e396ccbb889fcf
Koma92awich <koma92awich@gmail.com>
2019-10-17 18:19:59 -0500

    Add files via upload

---
+++ W3-L8-HOMEWORK/Day2/Problem2/part i/MathRandom.java
@@ -0,0 +1,14 @@
+package lesson8Labs;
+
+import java.util.function.Supplier;
+
+public class MathRandom {
+
+	public static void main(String[] args) {
+
+		Supplier<Double> sp = Math::random; 
+
+		System.out.println(sp.get());
+	}
+
+}
