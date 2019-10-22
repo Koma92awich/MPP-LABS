d78c7d1a43e4f86b2654b0c18d70ef5567273dbd
Koma92awich <koma92awich@gmail.com>
2019-10-22 13:31:36 -0500

    Update Problem6.java

--- W4-L9-HOMEWORK/problem6/Problem6.java
+++ W4-L9-HOMEWORK/problem6/Problem6.java
@@ -8,9 +8,17 @@ public class Problem6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		List<Set<String>> list = new ArrayList<>();
+		Set<String> set1 = new LinkedHashSet<>(Arrays.asList("A","B"));
+		Set<String> set2 = new LinkedHashSet<>(Arrays.asList("D"));
+		Set<String>set3 = new LinkedHashSet<>(Arrays.asList("1","3","5"));
+		list.add(set1);
+		list.add(set2);
+		list.add(set3);
+		System.out.println(union(list));
 
 	}
-	public Set<String> union(List<Set<String>> sets){
+	public static Set<String> union(List<Set<String>> sets){
 		
 	Optional<Set<String>> un =	sets.stream().reduce((x,y) ->{
 			x.addAll(y);
